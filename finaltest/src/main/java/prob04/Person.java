e4e447bd3cd1d4b061369d472ad566e66e172f66
6loss0m <42835988+6loss0m@users.noreply.github.com>
2023-08-16 17:01:33 +0900

    [ADD DAY6] final test code

---
+++ finaltest/src/main/java/prob04/Person.java
@@ -0,0 +1,28 @@
+package prob04;
+
+public class Person {
+	private static int numberOfPerson; // 전체 인구수
+	private int age;
+	private String name;
+
+	public Person(String name) {
+		this.name = name;
+		this.age = 12;
+		numberOfPerson++;
+	}
+
+	public Person(int age, String name) {
+		this.name = name;
+		this.age = age;
+		numberOfPerson++;
+	}
+
+	public static int getPopulation() {
+		return numberOfPerson;
+	}
+
+	public void selfIntroduce() {
+		System.out.println("내 이름은 " + name + "이며, 나이는" + age + "입니다.");
+	}
+
+}
